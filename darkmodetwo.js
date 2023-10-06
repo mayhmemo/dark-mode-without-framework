4f14d67d2a42a4cd12af4e16d71f410304e9c4b4
Mayhm <nisekoimylove@gmail.com>
2023-10-06 11:05:02 -0300

    refactor: cleaner and no html call code

--- darkmodetwo.js
+++ darkmodetwo.js
@@ -1,40 +1,42 @@
-function onloads() {
-    var darkmode = localStorage.getItem("Darkmode");
-    const html = document.getElementById("html");
-    localStorage.setItem("Vision", 1);
-    var vision = localStorage.getItem("Vision");
-    if (darkmode == "true") {
-        html.classList.add("dark");
-        localStorage.setItem("Darkmode", true);
-        vision = parseInt(vision);
-        vision = vision + 1;
-        localStorage.setItem("Vision", vision);
-    } else if (darkmode == "false" || darkmode == null) {
-        html.classList.remove("dark");
-        localStorage.setItem("Darkmode", false);
+document.addEventListener("DOMContentLoaded", (event) => {
+    const html = document.documentElement;
+    
+    const themeToogle = document.querySelector('#themeToogle');
+
+    verifyTheme(html);
+    
+    themeToogle.addEventListener('click', function () {
+        const theme = localStorage.getItem('theme')
+        changeTheme(theme, html)
+    });
+});
+
+function changeTheme(theme, html){
+    if (theme === 'light') {
+        toogleToDark(html);
+    } else {
+        toogleToLight(html);
     }
 }
 
-function variable() {
-    var vision = localStorage.getItem("Vision");
-    if (vision == "1") {
-        localStorage.setItem("Darkmode", false);
+function verifyTheme(html){
+    const theme = localStorage.getItem('theme');
+
+    if (theme === 'light') {
+        toogleToLight(html);
+    } else if (theme === 'dark'){
+        toogleToDark(html);
+    } else {
+        localStorage.setItem('theme', 'light');
     }
 }
 
-function dark() {
-    var darkmode = localStorage.getItem("Darkmode");
-    const html = document.getElementById("html");
-    var vision = localStorage.getItem("Vision");
-    vision = parseInt(vision);
-    vision = vision + 1;
-    localStorage.setItem("Vision", vision);
+function toogleToDark(html){
+    localStorage.setItem('theme', 'dark');
+    html.classList.add("dark");
+}
 
-    if (darkmode == "true") {
-        html.classList.remove("dark");
-        localStorage.setItem("Darkmode", false);
-    } else if (darkmode == "false") {
-        html.classList.add("dark");
-        localStorage.setItem("Darkmode", true);
-    }
+function toogleToLight(html){
+    localStorage.setItem('theme', 'light');
+    html.classList.remove("dark");
 }
